0dd44ef45cb8533b2e923005c5e8759acac0e718
Pierson Brannan <plbrannan95@gmail.com>
2019-01-18 15:29:42 -0500

    index.js boilerplate

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const hbs = require("hbs");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.set("view engine", "hbs");
+app.use(methodOverride("_method"));
+
+app.use(require("./routes/index.js"));
+
+app.listen(3000, () => console.log("server is running"));
