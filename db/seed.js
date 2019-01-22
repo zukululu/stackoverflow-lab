27af67ed16b658e02ce0d80602174cf554869606
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-01-22 14:02:41 -0500

    debug

--- db/seed.js
+++ db/seed.js
@@ -1,73 +1,73 @@
-const User = require("../models/User");
-const { Question } = require("../models/Question");
-const bcrypt = require("bcrypt-nodejs")
+// const User = require("../models/User");
+// const { Question } = require("../models/Question");
+// const bcrypt = require("bcrypt-nodejs")
 
-User.find({}).remove(() => {
-    Question.find({}).remove(() => {
-      let bugs = User.create({
-          email: "bugsbgtunny@gmail.com",
-      }).then(user => {
-        Promise.all([
-          Question.create({
-            content: "eh, what's up doc?",
-            author: user._id
-          }).then(question => {
-            user.questions.push(question);
-          }),
-          Question.create({
-            content: "That's all, folks!",
-            author: user._id
-          }).then(question => {
-            user.questions.push(question);
-          })
-        ]).then(() => {
-          user.save(err => console.log(err));
-        });
-      });
+// User.find({}).remove(() => {
+//     Question.find({}).remove(() => {
+//       let bugs = User.create({
+//           email: "bugsbgtunny@gmail.com",
+//       }).then(user => {
+//         Promise.all([
+//           Question.create({
+//             content: "eh, what's up doc?",
+//             author: user._id
+//           }).then(question => {
+//             user.questions.push(question);
+//           }),
+//           Question.create({
+//             content: "That's all, folks!",
+//             author: user._id
+//           }).then(question => {
+//             user.questions.push(question);
+//           })
+//         ]).then(() => {
+//           user.save(err => console.log(err));
+//         });
+//       });
   
-      let daffy = User.create({
-          email: "daffyduck@gmail.com",
-      }).then(user => {
-        Promise.all([
-          Question.create({
-            content: "Who's this Duck Dodgers any how?",
-            author: user._id
-          }).then(question => {
-            user.questions.push(question);
-          }),
-          Question.create({
-            content: "You're dethpicable.",
-            author: user._id
-          }).then(question => {
-            user.questions.push(question);
-          })
-        ]).then(() => {
-          user.save(err => console.log(err));
-        });
-      });
+//       let daffy = User.create({
+//           email: "daffyduck@gmail.com",
+//       }).then(user => {
+//         Promise.all([
+//           Question.create({
+//             content: "Who's this Duck Dodgers any how?",
+//             author: user._id
+//           }).then(question => {
+//             user.questions.push(question);
+//           }),
+//           Question.create({
+//             content: "You're dethpicable.",
+//             author: user._id
+//           }).then(question => {
+//             user.questions.push(question);
+//           })
+//         ]).then(() => {
+//           user.save(err => console.log(err));
+//         });
+//       });
   
-      let elmer = User.create({
-          email: "elmerfudd@gmail.com",
-      }).then(user => {
-        Promise.all([
-          Question.create({
-            content:
-              "Shh. Be vewy vewy quiet. I'm hunting wabbits! Huh-huh-huh-huh!",
-            author: user._id
-          }).then(question => {
-            user.questions.push(question);
-          }),
+//       let elmer = User.create({
+//           email: "elmerfudd@gmail.com",
+//       }).then(user => {
+//         Promise.all([
+//           Question.create({
+//             content:
+//               "Shh. Be vewy vewy quiet. I'm hunting wabbits! Huh-huh-huh-huh!",
+//             author: user._id
+//           }).then(question => {
+//             user.questions.push(question);
+//           }),
   
-          Question.create({
-            content: "Kiww da wabbit!",
-            author: user._id
-          }).then(question => {
-            user.questions.push(question);
-          })
-        ]).then(() => {
-          user.save(err => console.log(err));
-        });
-      });
-    });
-  });
+//           Question.create({
+//             content: "Kiww da wabbit!",
+//             author: user._id
+//           }).then(question => {
+//             user.questions.push(question);
+//           })
+//         ]).then(() => {
+//           user.save(err => console.log(err));
+//         });
+//       });
+//     });
+//   });
   
