99ea8b1dcd8471009f93274b04bb802665886cf0
Pierson Brannan <plbrannan95@gmail.com>
2019-01-22 10:29:37 -0500

    Controllers added testing functionality

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/tweeter");
+mongoose.connect("mongodb://localhost/questionsapp");
 mongoose.Promise = Promise;
 module.exports = mongoose
