35423e50c9cad01549023775b42166ecc79e7d44
Pierson Brannan <plbrannan95@gmail.com>
2019-01-24 10:36:48 -0500

    Showing textarea for answer creating in controller/sanswer.js, but does not display author

--- controllers/answer.js
+++ controllers/answer.js
@@ -3,18 +3,26 @@ const User = require("../models/User");
 const bodyParser = require("body-parser");
 
 module.exports = {
-    // show: (req, res) => {},
-    // showing the answers is being handled within question.js
-    new: (req, res) => {
-
-    },
-    create: (req, res) => {
-
-    },
-    update: (req, res) => {
-
-    },
-    // delete: (req, res) => {}
-    // do we need delete?
-
-}
+  // show: (req, res) => {},
+  // showing the answers is being handled within question.js
+  new: (req, res) => {
+    Question.find({}).then(q => {
+      res.render("question/new", { q });
+    });
+  },
+  create: (req, res) => {
+    Answer.create({
+      content: req.body.content,
+      author: req.body.author
+    }).then(a => {
+      Question.findOne({ _id: req.params.id }).then(user => {
+        user.answers.push(a);
+        res.redirect("/question/${question._id");
+        // Don't think we need the res.redirect, but there it is
+      });
+    });
+  },
+  update: (req, res) => {}
+  // delete: (req, res) => {}
+  // do we need delete?
+};
