4d4065022a005d8d82e35e4eac1b3c52b22b90c2
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-01-24 12:05:24 -0500

    updated user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,43 @@
 const User = require('../models/User')
+const { Question } = require('../models/Question')
+const passport = require('passport')
 
 module.exports = {
   show: (req,res) => {
-    User.findById(req.params.id)
+    User.findOne({ _id: req.params.id})
       .populate({
         path: 'questions',
-        options: { limit: 5, sort: {createdAt: -1 } }
+        options: { limit: 5, sort: { createdAt: -1 } }
+      }).then(u => {
+        res.render('user/show', { u })
       })
   },
-  new: (req,res) => {
-    res.render('user/new')
+  login: (req,res) => {
+    res.render('user/login', { message: req.flash('loginMessage') })
   },
-  create: (req,res) => {
-    User.create({
-      email: req.body.email,
-      password: req.body.password
-    }).then(user => {
-      res.redirect(`user/${user._id}`)
+  createLogin: (req,res) => {
+    const login = passport.authenticate('local-login', {
+      successRedirect: '/',
+      failureRedirect: '/user/login',
+      failureFlash: true
     })
+
+    return login(req,res)
+  },
+  signUp: (req,res) => {
+    res.render('user/signup', { message: req.flash('signupMessage') })
+  },
+  createSignUp: (req,res) => {
+    const signup = passport.authenticate('local-signup', {
+      successRedirect: '/',
+      failureRedirect: '/signup',
+      failureFlash: true
+    })
+
+    return signup(req, res)
+  },
+  logout: (req,res) => {
+    req.logout()
+    res.redirect('/')
   }
 }
