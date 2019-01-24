15a70d49dd3befade0ad49fe70292675f4e31165
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-01-24 12:35:31 -0500

    updated views

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const hbs = require("hbs");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const app = express();
-const routes = require("./routes/index.js");
+// const express = require("express");
+// const hbs = require("hbs");
+// const bodyParser = require("body-parser");
+// const methodOverride = require("method-override");
+// const app = express();
+// const routes = require("./routes/index.js");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
-app.use("/", routes);
-app.set("view engine", "hbs");
-app.use(methodOverride("_method"));
+// app.use(express.static("public"));
+// app.use("/", routes);
+// app.set("view engine", "hbs");
+// app.use(methodOverride("_method"));
 
-app.use(require("./routes/index.js"));
+// app.use(require("./routes/index.js"));
 
-app.listen(3000, () => {
-  console.log("server is running");
-});
+// app.listen(3000, () => {
+//   console.log("server is running");
+// });
