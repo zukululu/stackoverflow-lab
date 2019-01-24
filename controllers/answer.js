e41e7553ffd9b505fc0092ca3f3ef0b2c249eeba
Pierson Brannan <plbrannan95@gmail.com>
2019-01-24 10:19:51 -0500

    controllers/answer.js made with skeleton

---
+++ controllers/answer.js
@@ -0,0 +1,20 @@
+const { Question, Answer } = require("../models/Question");
+const User = require("../models/User");
+const bodyParser = require("body-parser");
+
+module.exports = {
+    // show: (req, res) => {},
+    // showing the answers is being handled within question.js
+    new: (req, res) => {
+
+    },
+    create: (req, res) => {
+
+    },
+    update: (req, res) => {
+
+    },
+    // delete: (req, res) => {}
+    // do we need delete?
+
+}
